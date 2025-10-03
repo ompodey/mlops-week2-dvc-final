9e7a1a65dff41c37d58ff5f1e294ef66ef6035ee
om Podey <84179156+ompodey@users.noreply.github.com>
2025-10-03 15:47:09 +0530

    Create augment_data.py

---
+++ augment_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.read_csv("iris.csv")
+datav2 = data.sample(n=50, random_state=42)
+data = pd.concat([data, datav2], ignore_index=True)
+data.to_csv("irisv2.csv", index=False)
+
+print(f"New dataset rows: {len(data)}")
+
